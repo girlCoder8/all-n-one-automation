e253f7e1efee22cb22f969f40dbff83fb7299f73
girlCoder8 <62305157+girlCoder8@users.noreply.github.com>
2023-06-26 23:00:47 -0400

    init commit

---
+++ mobile/static/constants.ts
@@ -0,0 +1,6 @@
+export const APP_PACKAGE = "io.appium.android.apis";
+export const APP_HEADER = "API Demos";
+export const COMMAND_TWO_POPUP_MSG = "You selected: 1 , Command two";
+export const MENU_ITEMS = ['API Demos', 'Accessibility', 'Animation', 'App', 'Content', 'Graphics', 'Media', 'NFC', 'OS', 'Preference', 'Text', 'Views'];
+export const ALERT_TEXT = `You are using a Motorola Razr+ phone`
+export const WALLPAPER_TEXT = 'Example of how you can make an activity have a translucent background';
